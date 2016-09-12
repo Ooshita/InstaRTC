852e2222b43aecb360315ace6ddfb395b7fb7f12
Noriaki Oshita <ponkichi0213@gmail.com>
2016-09-13 02:11:37 +0900

    json_decode()にtrueの記述を忘れていたので記述した

--- app/getJson.php
+++ app/getJson.php
@@ -2,5 +2,5 @@
 //getenv('instagram')で環境変数内にあるaccess_tokenを取得する
 $userApiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='.getenv('instagram');
 // JSON($json)を連想配列に変換(デコード)する
-$array = json_decode(@file_get_contents($userApiUrl));
+$array = json_decode(@file_get_contents($userApiUrl),true);
 echo var_dump($array);
