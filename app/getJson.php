c5a398c6537da8a809a74a43c1954b647430e6f2
Noriaki Oshita <ponkichi0213@gmail.com>
2016-09-13 01:14:15 +0900

    インスタグラムのjsonを取得するgetJson.phpの作成。自分の最近の投稿を取得するところまで実装。

---
+++ app/getJson.php
@@ -0,0 +1,6 @@
+<?php
+//getenv('instagram')で環境変数内にあるaccess_tokenを取得する
+$userApiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='.getenv('instagram');
+// JSON($json)を連想配列に変換(デコード)する
+$array = json_decode(@file_get_contents($userApiUrl));
+echo var_dump($array);
--- app/videochat.js
+++ app/videochat.js
@@ -5,7 +5,7 @@ var localStream;    // 自分の映像ストリームを保存しておく変数
 var connectedCall;  // 接続したコールを保存しておく変数
  
 // SkyWayのシグナリングサーバーへ接続する (APIキーを置き換える必要あり）
-var peer = new Peer({ key: '************************************', debug: 3});
+var peer = new Peer({ key: $ENV{api_key}, debug: 3});
  
 // シグナリングサーバへの接続が確立したときに、このopenイベントが呼ばれる
 peer.on('open', function(){
